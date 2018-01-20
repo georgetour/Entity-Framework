3703befbf24adb9b9d4f3ee8328ef234ce61705c
George Tourtsinakis <george@besmartbesimple.com>
2018-01-20 20:36:57 +0200

    Seed method and dummy data

--- Chapter 4 Code-first workflow/CodeFirstExistingDatabase/CodeFirstExistingDatabase/Migrations/Configuration.cs
+++ Chapter 4 Code-first workflow/CodeFirstExistingDatabase/CodeFirstExistingDatabase/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 namespace CodeFirstExistingDatabase.Migrations
 {
     using System;
+    using System.Collections.ObjectModel;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,18 +15,17 @@ namespace CodeFirstExistingDatabase.Migrations
 
         protected override void Seed(CodeFirstExistingDatabase.PlutoContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            context.Authors.AddOrUpdate(a => a.Name,
+                new Author
+                {
+                    Name = "Author 1",
+                    Courses = new Collection<Course>()
+                    {
+                        new Course() {Name = "Course for author 1",Description = "Description" }
+                    }
+                }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+                );
         }
     }
 }
