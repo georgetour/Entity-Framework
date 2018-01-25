ec9d9e4cca5040f55466c5a443e6b9651f786d5c
George Tourtsinakis <george@besmartbesimple.com>
2018-01-25 15:41:27 +0200

    Advaned FluentAPI and new exercise to do

---
+++ Chapter 5 Overriding Conventions/FluentAPI/FluentAPI/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAPI.EntityConfigurations
+{
+    public class CourseConfiguration : EntityTypeConfiguration<Course>
+    {
+        public CourseConfiguration()
+        {
+           
+                       Property(c => c.Name)
+                       .IsRequired()
+                       .HasMaxLength(255);
+
+            //Change max length for Description in Courses
+           
+                        Property(c => c.Description)
+                        .HasMaxLength(2000);
+
+            //Change Courses foreign key Author_Id to AuthorId
+            //And if an author has a course not let to be delete
+           
+                        HasRequired(c => c.Author)
+                        .WithMany(a => a.Courses)
+                        .HasForeignKey(c => c.AuthorId)
+                        .WillCascadeOnDelete(false);
+
+            //Change name of table TagCourses(was created in database automatically) to CoursesTag
+           
+                        HasMany(c => c.Tags)
+                        .WithMany(c => c.Courses)
+                        .Map(m =>
+                        {
+                            m.ToTable("CourseTags");
+                            //Since we start from Course the left side is Course
+                            //The name of the left key is going to be CourseId
+                            m.MapLeftKey("CourseId");
+                            //The name of the left key is going to be TagId
+                            m.MapRightKey("TagId");
+                        });
+
+            //New table cover and relationship with Course
+           
+                        HasRequired(c => c.Cover)
+                        .WithRequiredPrincipal(c => c.Course);
+
+        }
+    }
+}
--- Chapter 5 Overriding Conventions/FluentAPI/FluentAPI/FluentAPIContext.cs
+++ Chapter 5 Overriding Conventions/FluentAPI/FluentAPI/FluentAPIContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAPI.EntityConfigurations;
 using System.Data.Entity;
 
 namespace FluentAPI
@@ -15,37 +11,7 @@ namespace FluentAPI
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Course>()
-                       .Property(c => c.Name)
-                       .IsRequired()
-                       .HasMaxLength(255);
-
-            //Change max length for Description in Courses
-            modelBuilder.Entity<Course>()
-                        .Property(c => c.Description)
-                        .HasMaxLength(2000);
-
-            //Change Courses foreign key Author_Id to AuthorId
-            //And if an author has a course not let to be delete
-            modelBuilder.Entity<Course>()
-                        .HasRequired(c => c.Author)
-                        .WithMany(a => a.Courses)
-                        .HasForeignKey(c => c.AuthorId)
-                        .WillCascadeOnDelete(false);
-
-            //Change name of table TagCourses(was created in database automatically) to CoursesTag
-            modelBuilder.Entity<Course>()
-                        .HasMany(c => c.Tags)
-                        .WithMany(c => c.Courses)
-                        .Map(m => m.ToTable("CourseTags"));
-
-            //New table cover and relationship with Course
-            modelBuilder.Entity<Course>()
-                        .HasRequired(c => c.Cover)
-                        .WithRequiredPrincipal(c => c.Course);
-
-
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.Add(new CourseConfiguration());
         }
 
     }
